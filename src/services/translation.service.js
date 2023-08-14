c1445c4058921b1289be8030c13af094aee98a74
Candyzorua <jiaying.ong2@gmail.com>
2023-08-14 22:42:45 +0800

    Added tone marks to pinyin

--- src/services/translation.service.js
+++ src/services/translation.service.js
@@ -1,13 +1,15 @@
 const Translation = require('../models/translation.model');
+const toneConvert = require('pinyin-tone-convert')
 
 /**
  * Translate text from Mandarin to English
  * @param {string} text Mandarin text to translate
  * @param {string} type Simplified or traditional
- * @returns {Promise<string|null>} 
+ * @returns {Promise<Object|null>} 
  */
 const translateText = async (text, type) => {
     const translationData = await Translation.findOne({ [type]: text });
+    translationData.pinyin ? translationData.pinyin = toneConvert(translationData.pinyin) : null;
     return translationData;
 };
 
